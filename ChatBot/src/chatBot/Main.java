ed4e23c7dbb62de0f264fb0c8a6f26038a3f034b
Caychax <80284638+Caychax@users.noreply.github.com>
2021-04-10 10:28:42 +0100

    Removed code from main

--- ChatBot/src/chatBot/Main.java
+++ ChatBot/src/chatBot/Main.java
@@ -1,41 +1,11 @@
 package chatBot;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.*;
-
-import org.json.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		
-		String apiKey = "e3d8cd2bc42e09a996101d712189ec71";
-		String location = "Brisbane,AU";
-	
-		URL oracle = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid="+apiKey+"&units=metric");
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
-		
-		String input = in.readLine();
-		
-		JSONObject brisbane = new JSONObject(input);
-		
-		String[] names = JSONObject.getNames(brisbane);
-		
-		List<String> nameList = new ArrayList<String>();
+	public static void main(String[] args){
 		
 		
-		
-		//JSONPointer pointer = new JSONPointer(names[0]);
-		
-		
-		
-		for(String string: names) {
-			System.out.println(string+" : "+brisbane.get(string));
-		}
 		
 	}
 
