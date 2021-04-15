4694cdc399ec9b76f8b36acc4846c9ef9fa77c07
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-04-15 16:17:06 +0100

    Testing the actual temperature in junit test

--- ChatBot/src/chatBot/WeatherTest.java
+++ ChatBot/src/chatBot/WeatherTest.java
@@ -2,13 +2,22 @@ package chatBot;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 class WeatherTest {
-
+	
+	//testing the actual temperature 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetActualTemp() throws IOException {
+		String location = "Brisbane,AU";
+		WeatherData data = new WeatherData(location);
+		Weather test = new Weather(data);
+		
+		double expected = 17.91; //the expected value might not be same all the time as the weather keeps changing
+		double actual = test.getActualTemp();
+		assertEquals (expected, actual, 0.1); //equating the value with the epsilon of 0.1
 	}
 
 }
