0d586af3a2f1ae4231f29c51584bee015eff5afb
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-04-15 15:47:50 +0100

    Testing the Main, which is JSON object inside of a JSONObject in juni test

--- ChatBot/src/chatBot/WeatherDataTest.java
+++ ChatBot/src/chatBot/WeatherDataTest.java
@@ -187,7 +187,7 @@ class WeatherDataTest {
 		String expected="{\"coord\":{\"lon\":153.0281,\"lat\":-27.4679},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01n\"}],\"base\":\"stations\",\"main\":{\"temp\":20.99,\"feels_like\":21.18,\"temp_min\":20,\"temp_max\":22.22,\"pressure\":1016,\"humidity\":78},\"visibility\":10000,\"wind\":{\"speed\":3.6,\"deg\":360},\"clouds\":{\"all\":0},\"dt\":1618481760,\"sys\":{\"type\":1,\"id\":9485,\"country\":\"AU\",\"sunrise\":1618430660,\"sunset\":1618471873},\"timezone\":36000,\"id\":2174003,\"name\":\"Brisbane\",\"cod\":200}";
 		String actual = test.getInput();
 		
-		assertEquals(expected, actual); //the expected values keeps on updating so the expected value won't be this in the future 		
+		assertEquals(expected, actual); //the expected values keeps on updating because the weather changes continuously 		
 	}
 	
 	//testing json object created from input
@@ -198,9 +198,20 @@ class WeatherDataTest {
 		
 		String expected="{\"visibility\":10000,\"timezone\":36000,\"main\":{\"temp\":18.92,\"temp_min\":16.67,\"humidity\":77,\"pressure\":1016,\"feels_like\":18.88,\"temp_max\":20},\"clouds\":{\"all\":0},\"sys\":{\"country\":\"AU\",\"sunrise\":1618430660,\"sunset\":1618471873,\"id\":9485,\"type\":1},\"dt\":1618493703,\"coord\":{\"lon\":153.0281,\"lat\":-27.4679},\"weather\":[{\"icon\":\"01n\",\"description\":\"clear sky\",\"main\":\"Clear\",\"id\":800}],\"name\":\"Brisbane\",\"cod\":200,\"id\":2174003,\"base\":\"stations\",\"wind\":{\"deg\":310,\"speed\":2.57}}";
 		JSONObject actual = test.getLocal();
-		String actual2 = actual.toString();
 		
-		assertEquals(expected, actual); //the expected values keeps on updating so the expected value won't be this in the future 		
+		assertEquals(expected, actual); //the expected values keeps on updating because the weather changes continuously  		
+	}
+	
+	//testing the main, which is a JSON object inside of a JSONObject
+	@Test
+	void testGetMain() throws IOException {
+		String location = "Brisbane,AU";
+		WeatherData test = new WeatherData(location);
+		
+		String expected="{\"temp\":19.03,\"temp_min\":17.78,\"humidity\":82,\"pressure\":1016,\"feels_like\":19.13,\"temp_max\":20}";
+		JSONObject actual = test.getMain();
+		
+		assertEquals(expected, actual); //the expected values keeps on updating because the weather changes continuously 
 	}
 		
 }
