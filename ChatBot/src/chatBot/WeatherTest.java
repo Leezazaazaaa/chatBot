9fca179bbbc8656dfbdc36eb0dc2579da047517a
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-04-15 16:20:07 +0100

    Testing the minimum temperature in junit test

--- ChatBot/src/chatBot/WeatherTest.java
+++ ChatBot/src/chatBot/WeatherTest.java
@@ -19,5 +19,17 @@ class WeatherTest {
 		double actual = test.getActualTemp();
 		assertEquals (expected, actual, 0.1); //equating the value with the epsilon of 0.1
 	}
+	
+	//testing the minimum temperature
+	@Test
+	void testGetMinTemp() throws IOException {
+		String location = "Brisbane,AU";
+		WeatherData data = new WeatherData(location);
+		Weather test = new Weather(data);
+		
+		double expected = 16.67; //the expected value might not be same all the time as the weather keeps changing
+		double actual = test.getMinTemp();
+		assertEquals (expected, actual, 0.1);  //equating the value with the epsilon of 0.1
+	}
 
 }
