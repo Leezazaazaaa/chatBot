a50e1201fa5c4a5a9b01c022438f3e0ce79d7942
lukas <lukas@192.168.1.23>
2021-04-29 16:37:12 +0100

    Added comments

--- ChatBot/src/chatBot/ChatBot.java
+++ ChatBot/src/chatBot/ChatBot.java
@@ -12,42 +12,42 @@ public class ChatBot  extends Weather{
 		
 	}
 
-	public void createFile() {		
+	public void createFile() {		//Creates a new file
 	    try {
-	    	File file = new File("responses.txt");
-	        if (file.createNewFile()) {
-	          System.out.println("File created: " + file.getName());
-	        } else {
-	          System.out.println("File already exists.");
+	    	File file = new File("responses.txt");	//the file's location will be stored as file
+	        if (file.createNewFile()) {		//checks if that file was just created
+	          System.out.println("File created: " + file.getName());	//outputs a confirmation message
+	        } else {										//else if file was not created
+	          System.out.println("File already exists.");	//file already exists
 	        }
-	      } catch (IOException e) {
+	      } catch (IOException e) {						//catches an error
 	        System.out.println("An error occurred.");
 	        e.printStackTrace();
 	      }		
 	}
 	
-	public String[] Responses() {
-		String[] responses = {"hello - Hi there! How are you?", 
-						 	  "good - That's good to hear!",
+	public String[] Responses() {	//temporary method for how the user's input and ChatBot's responses are managed
+		String[] responses = {"hello - Hi there! How are you?", //String array holds all inputs and outputs, separated by a semi-colon
+						 	  "good - That's good to hear!",		
 						 	  "what is your name? - My name is ChatBot",
 						 	  "help - Enter input into the console and receive a response.",
 						 	  "bye - Goodbye!",
 						 	  "what is the weather? - it is (answer)",
 						 	  "what should i wear? - You Should Wear (answer)"};
-		return responses;
+		return responses;	//returns the full String array
 	}
 	
-	public void setResponses() {
+	public void setResponses() {	//writes inputs and outputs into the text file responses
 		String[] responses = Responses();
 		
 		
 	    try {
 	        FileWriter myWriter = new FileWriter("responses.txt");
-	        for(int i = 0; i < responses.length; i++) {
-	        myWriter.write(responses[i] + "\n");
+	        for(int i = 0; i < responses.length; i++) {	//to loop through all indexes of the String array
+	        myWriter.write(responses[i] + "\n");	//writes into file 
 	        }
-	        myWriter.close();
-	        System.out.println("Successfully wrote to the file.");
+	        myWriter.close();	//closes file
+	        System.out.println("Successfully wrote to the file.");	//confirmation message
 	      } catch (IOException e) {
 	        System.out.println("An error occurred.");
 	        e.printStackTrace();
@@ -55,19 +55,19 @@ public class ChatBot  extends Weather{
 		
 	}
 	
-    public static void displayMenu(boolean startup) {		
+    public static void displayMenu(boolean startup) {		//displays greeting
         if(startup) {											
             System.out.println("Please enter a command.");		
         }
         System.out.print("> ");									
     }
     
-    public static int getLines(String filename) {				
+    public static int getLines(String filename) {		//gets the total amount of lines in filename		
         int lines = 0;
         
         try(BufferedReader br = new BufferedReader(			
                 new FileReader(filename))) {
-            while(br.readLine() != null) lines++;
+            while(br.readLine() != null) lines++;	//lines increases as it recognizes each line in text file
         } catch(IOException exc) {
             System.out.println("I/O Exception: " + exc);
         }
@@ -75,7 +75,7 @@ public class ChatBot  extends Weather{
         return lines;
     }
     
-    public static String getUserInput() {
+    public static String getUserInput() {	//gets the user's input
         String userInput = null;
         
         try {
@@ -87,24 +87,24 @@ public class ChatBot  extends Weather{
             System.out.println("I/O Exception: " + exc);
         }
         
-        return userInput;
+        return userInput;		//returns user's input
     }
     
-    public static String[] getResponsesArray(String filename, int lines) {
+    public static String[] getResponsesArray(String filename, int lines) {	//retrieves all responses from text file
         int lineCount = 0;
         
         String line;
-        String[] responsesArray = new String[lines];
+        String[] responsesArray = new String[lines];	//new String array created with size of lines(amount of lines in text file)
         
         try(BufferedReader br = new BufferedReader(
                 new FileReader(filename))) {
             
             do {
-                line = br.readLine();
+                line = br.readLine();	//stores each line into String line
                 
-                if(line != null) {
-                    responsesArray[lineCount] = line;
-                    lineCount++;
+                if(line != null) {		//checks if that line is not empty
+                    responsesArray[lineCount] = line;	//adds that line into array
+                    lineCount++;				
                 }
             } while(line != null);
         } catch(FileNotFoundException exc) {
@@ -113,50 +113,50 @@ public class ChatBot  extends Weather{
             System.out.println("I/O Exception: " + exc);
         }
         
-        return responsesArray;
+        return responsesArray;	//returns new array with all responses
     }
     
-    public static String getResponse(String[] responses, String userInput) {
+    public static String getResponse(String[] responses, String userInput) {	//Gets chatbot's responses given the user-defined input command
         String tag, response;
         String[] array;
         
         for(String responseLine: responses) {
-            if(responseLine != null) {
-                array = responseLine.split(" - ");				
+            if(responseLine != null) {	
+                array = responseLine.split(" - ");		//User's input and chatBot's output is separated by semi-colon
                 tag = array[0];									
                 response = array[1];					
                 
-                if(tag.compareToIgnoreCase(userInput) == 0) {	
-                    return response;							
+                if(tag.compareToIgnoreCase(userInput) == 0) {	//finds valid response
+                    return response;							//returns response		
                 }
             }
         }
         
-        return "No response...";								
+        return "No response...";							//else returns No Response...		
     }
     
-    public void startChatBot() {
+    public void startChatBot() {		//Start chatBot
         String userInput, response, temp, currentWeather;
         String filename = "responses.txt";     
         
         int lines = getLines(filename);
         String[] responsesArray = getResponsesArray(filename, lines);
-        displayMenu(true);
+        displayMenu(true);					//displays greeting
         
         do {
-            userInput = getUserInput();
+            userInput = getUserInput();		//sets user's input as userInput
             
-            response = getResponse(responsesArray, userInput);
+            response = getResponse(responsesArray, userInput);	//retrieves correct response to user's input
             
-            if(userInput.contains("weather")) {
-            	if(getActualTemp() >= 15) {
-            		currentWeather = String.valueOf(getActualTemp());
-            		temp = response.replace("(answer)", currentWeather);
-            		response = temp;
+            if(userInput.contains("weather")) {			//checks if user is asking for weather
+            	if(getActualTemp() >= 15) {				//checks the temperature(not necessary, but will be useful later)
+            		currentWeather = String.valueOf(getActualTemp());	//sets the data into a String
+            		temp = response.replace("(answer)", currentWeather);	//changes (answer) to correct piece of data
+            		response = temp;	
             	}
             }
             
-            System.out.println("Chatbot: " + response);
+            System.out.println("Chatbot: " + response);	//outputs the full response
 
             if(!userInput.equals("bye")) {
                 displayMenu(false);			//display will turn off when "bye" is said
