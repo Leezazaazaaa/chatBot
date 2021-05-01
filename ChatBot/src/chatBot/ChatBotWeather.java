7136941186aba392c91e2e0ddab9f0a91c75736c
lukas <lukas@192.168.1.23>
2021-05-01 12:29:16 +0100

    Added method in ChatBotWeather class "outputValidation" which validates the data retrieved from WeatherAPI

--- ChatBot/src/chatBot/ChatBotWeather.java
+++ ChatBot/src/chatBot/ChatBotWeather.java
@@ -6,4 +6,32 @@ public class ChatBotWeather extends ChatBot{
 		super(data);
 	}
 	
+	public String outputValidation(String response, String userInput) {	//Validation of data retrieved from WeatherAPI
+		String currentWeather, temp;
+		if(userInput.contains("weather")) {
+    		currentWeather = String.valueOf(getDescription() );	//sets the data into a String
+    		temp = response.replace("(answer)", currentWeather);	//changes (answer) to correct piece of data
+    		response = temp;
+		}
+		else if(userInput.contains("temperature")) {
+    		currentWeather = String.valueOf(getActualTemp() );	//sets the data into a String
+    		temp = response.replace("(answer)", currentWeather);	//changes (answer) to correct piece of data
+    		response = temp;
+		}
+		else if(userInput.contains("humidity")){
+    		currentWeather = String.valueOf(getHumidity() );	//sets the data into a String
+    		temp = response.replace("(answer)", currentWeather);	//changes (answer) to correct piece of data
+    		response = temp;
+		}
+		else if(userInput.contains("pressure")){
+    		currentWeather = String.valueOf(getPressure() );	//sets the data into a String
+    		temp = response.replace("(answer)", currentWeather);	//changes (answer) to correct piece of data
+    		response = temp;
+		}
+		if(userInput.contains("wear")) {
+			response = clothingSuggestion(response);
+		}
+		return response;
+	}
+	
 }
