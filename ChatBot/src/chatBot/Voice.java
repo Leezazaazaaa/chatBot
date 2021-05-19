4cb7967e755ed8d1262441fdd5923448023f9884
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-05-20 00:18:01 +0100

    Updated voice class to display weather info when given voice command for now it is the word "Weather"

--- ChatBot/src/chatBot/Voice.java
+++ ChatBot/src/chatBot/Voice.java
@@ -9,7 +9,7 @@ public class Voice {
 	 public static void main(String[] args) throws IOException {
 		 Configuration configuration = new Configuration();
 		 
-		 configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		 configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us"); //reference the data jar filebrary
 		 configuration.setDictionaryPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.dic");
 		 configuration.setLanguageModelPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.lm");
 		 
@@ -24,11 +24,21 @@ public class Voice {
 			 String work = null;
 			 System.out.println("Input Command is: " + command);
 		 
-			 if(command.equalsIgnoreCase("Open Safari")){
+			 if(command.equalsIgnoreCase("Hi")){
 				 work = "open -a Safari http://www.apple.com";
 				 
-			 }else if(command.equalsIgnoreCase("Close Safari")) {
-				 work = "killall Safari";
+			 }else if(command.equalsIgnoreCase("Weather")) {
+				 WeatherData d = new WeatherData("Brisbane,AU");//step1: Create a WeatherData object by sending a location
+					
+				//	d.display();//You can display its contents like so
+					
+					Weather info = new Weather(d);//Step 2: create a Weather Object from the WeatherData Object
+					
+					// you now have access to all the info you want.
+					System.out.println("\ncurrent temp: "+info.getActualTemp()+"C");
+					System.out.println("\nWeather Conditions: "+info.getDescription());
+					
+					
 			 }
 			 
 			 if(work != null) {
