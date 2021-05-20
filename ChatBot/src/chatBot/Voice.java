9525705d5848a63d1f83fae4fb0c3a647c16f02f
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-05-20 02:19:30 +0100

    Updated Voice command is converted to text and test is recognized by class TextSound and gives the output through sound for which sound is used kevin16

--- ChatBot/src/chatBot/Voice.java
+++ ChatBot/src/chatBot/Voice.java
@@ -9,41 +9,39 @@ public class Voice {
 	 public static void main(String[] args) throws IOException {
 		 Configuration configuration = new Configuration();
 		 
-		 configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us"); //reference the data jar filebrary
-		 configuration.setDictionaryPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.dic");
-		 configuration.setLanguageModelPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.lm");
+		 configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us"); //reference the data jar file
+		 configuration.setDictionaryPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.dic"); //path for dictionary
+		 configuration.setLanguageModelPath("/Users/leeza/git/chatbot2.0/ChatBot/src/VoiceCommands/4537.lm"); //path for pronounciation 
 		 
-		 LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
-		 
+		 LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration); //recognizes the voice imported from library
+		  
 		 recognize.startRecognition(true);
 		 
 		 SpeechResult speechResult;
 		 
-		 while((speechResult = recognize.getResult()) != null){
+		 while((speechResult = recognize.getResult()) != null){ //loops when the voice is recognized
 			 String command = speechResult.getHypothesis();
-			 String work = null;
 			 System.out.println("Input Command is: " + command);
 		 
-			 if(command.equalsIgnoreCase("Hi")){
-				 work = "open -a Safari http://www.apple.com";
+			 if(command.equalsIgnoreCase("Hi") && command.contains("Hi")){ //voice command is Hi
+				 TextSound tts = new TextSound("kevin16");
+				 String message = "Hello";
+				 tts.speak(message);
 				 
-			 }else if(command.equalsIgnoreCase("Weather")) {
+			 }else if(command.equalsIgnoreCase("Weather") || command.contains("Weather")) { //when the voice command is weather it will forecast thee weather
 				 WeatherData d = new WeatherData("Brisbane,AU");//step1: Create a WeatherData object by sending a location
-					
-				//	d.display();//You can display its contents like so
-					
 					Weather info = new Weather(d);//Step 2: create a Weather Object from the WeatherData Object
-					
 					// you now have access to all the info you want.
-					System.out.println("\ncurrent temp: "+info.getActualTemp()+"C");
+					System.out.println("\ncurrent temp: "+info.getActualTemp()+"C"); //prints out the weather condition 
 					System.out.println("\nWeather Conditions: "+info.getDescription());
-						
-			 }else if(command.equalsIgnoreCase("How are you ")) {
-				 
-			 }
-			 
-			 if(work != null) {
-				Runtime.getRuntime().exec(work);
+					TextSound tts = new TextSound("kevin16");
+					String message = "Current Temperature is " + info.getActualTemp() + "degree celcius and the weather condition is " + info.getDescription(); //weather information
+					tts.speak(message);
+					
+			 }else if(command.equalsIgnoreCase("How are you ")) { //voice command is how are you
+				 TextSound tts = new TextSound("kevin16");
+				 String message = "I am good, thank you";
+				 tts.speak(message);
 			 }
 		 }
 	 }
